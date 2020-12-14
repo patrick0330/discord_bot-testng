ccd4528475910b9b8c0d7211c435e60714eb54c1
patrick0330 <74896573+patrick0330@users.noreply.github.com>
2020-12-14 11:38:23 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import discord
+import json
 from discord.ext import commands
+with open('setting.json',mode = 'r',encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+
 intents = discord.Intents.default()
 intents.members = True
 
@@ -16,21 +20,21 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     # print(f"{member} join!")
-    channel = bot.get_channel(787873726376706058)
-    await channel.send(f'{member} join')
+    channel = bot.get_channel(int(jdata['JoinLeaveLog']))
+    await channel.send(f'{membem} join')
 
 @bot.event
 async def on_member_remove(member):
     print(f"{member} leave!")
-    channel = bot.get_channel(787873751844651050)
+    channel = bot.get_channel(int(jdata['JoinLeaveLog']))
     await channel.send(f'{member} leave')
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f'{ctx.author} onichan ohayo')
+    await ctx.send(f'{ctx.author.mention} onichan ohayo')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)}(ms)')
 
-bot.run('Nzg3NzUxNjcxNTM4Nzc4MTIy.X9Zg6g.lFTZwAoD2t9eXOyVNw9N86bsmQw')
+bot.run(jdata['TOKEN'])
