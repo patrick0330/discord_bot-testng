70a57b3b82d32d75f98e78b2003c780296bdaafa
patrick0330 <74896573+patrick0330@users.noreply.github.com>
2020-12-15 01:47:56 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import discord
-import json
 from discord.ext import commands
+
+import json
+import random
+import os
+
 with open('setting.json',mode = 'r',encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
@@ -14,27 +18,27 @@ bot = commands.Bot(command_prefix='!!',intents = intents)
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
-    
-
-
-@bot.event
-async def on_member_join(member):
-    # print(f"{member} join!")
-    channel = bot.get_channel(int(jdata['JoinLeaveLog']))
-    await channel.send(f'{membem} join')
-
-@bot.event
-async def on_member_remove(member):
-    print(f"{member} leave!")
-    channel = bot.get_channel(int(jdata['JoinLeaveLog']))
-    await channel.send(f'{member} leave')
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send(f'{ctx.author.mention} onichan ohayo')
+async def load(ctx,extension):
+    bot.load_extension(f'cmds.{extension}')
+    await ctx.send(f'Loaded {extension} done.')
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send(f'{round(bot.latency*1000)}(ms)')
+async def unload(ctx,extension):
+    bot.unload_extension(f'cmds.{extension}')
+    await ctx.send(f'Unloaded {extension} done.')
 
-bot.run(jdata['TOKEN'])
+@bot.command()
+async def reload(ctx,extension):
+    bot.reload_extension(f'cmds.{extension}')
+    await ctx.send(f'Reloaded {extension} done.')
+
+for filename in os.listdir('./cmds'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cmds.{filename[:-3]}')
+
+if __name__ == "__main__" :
+    bot.run(jdata['TOKEN'])
+
+
