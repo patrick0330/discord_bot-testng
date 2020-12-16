2608158b9c42c12ea8775baa77ee2d0e0d8c65c2
patrick0330 <74896573+patrick0330@users.noreply.github.com>
2020-12-16 15:48:26 +0800

    Update react.py

--- cmds/react.py
+++ cmds/react.py
@@ -1,10 +1,22 @@
 import discord
 import random
 import json
+import pyimgur
+import re
 from discord.ext import commands
 from core.classes import Cog_Extension
 with open('setting.json',mode = 'r',encoding='utf8') as jfile:
     jdata = json.load(jfile)
+CLIENT_ID = "16837f6cdd1e5a4"
+im = pyimgur.Imgur(CLIENT_ID)
+gif = im.get_album('RFnz4AQ')
+img = im.get_album('dqE0kVw')
+jojo = im.get_album('j3QcZgp')
+
+
+
+
+
 
 class React(Cog_Extension):
     @commands.command()
@@ -44,6 +56,36 @@ class React(Cog_Extension):
         gura = random.randint(1,26)
         rgura = discord.File(f'C:\\Users\\USER\\Documents\\GitHub\\discord_bot-testng\\gura\\{gura}.jpg')
         await ctx.send(file = rgura)
+
+    @commands.command()
+    async def rgif(self,ctx):
+        rangif = random.choice(gif.images)
+        find_id = re.findall('Image (.+?)>',str(rangif))
+        id = find_id[0]
+        image = im.get_image(id)
+        await ctx.send(image.link)
+
+    @commands.command()
+    async def rimg(self,ctx):
+        ranimg = random.choice(img.images)
+        find_id = re.findall('Image (.+?)>',str(ranimg))
+        id = find_id[0]
+        image = im.get_image(id)
+        await ctx.send(image.link)    
+    
+    @commands.command()
+    async def jojo(self,ctx):
+        rjojo = random.choice(jojo.images)
+        find_id = re.findall('Image (.+?)>',str(rjojo))
+        id = find_id[0]
+        image = im.get_image(id)
+        await ctx.send(image.link)
+    
+    @commands.command()
+    async def getavatar(self,ctx,*,member:discord.Member=None):
+        userAvatarUrl = member.avatar_url
+        await ctx.send(userAvatarUrl)
+
 
 def setup(bot):
     bot.add_cog(React(bot))
